4853dd03d7535dc568396a676ee0dbd4d77dbf37
mkizka <g.mkizka@gmail.com>
2024-05-09 00:42:01 +0900

    lex-cli

---
+++ scripts/generate.ts
@@ -0,0 +1,12 @@
+import { execSync } from "child_process";
+import { globSync } from "glob";
+
+const lexicons = [
+  ...globSync("./lexicons/**/*.json"),
+  ...globSync("./atproto/lexicons/com/atproto/**/*.json"),
+].join(" ");
+
+const $ = (command: string) => execSync(command, { stdio: "inherit" });
+
+$(`yes | pnpm lex gen-api ./app/generated/api ${lexicons}`);
+$(`yes | pnpm lex gen-server ./app/generated/server ${lexicons}`);
