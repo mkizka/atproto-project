889b25d6ba70c62e750250f3b370d1d847792d80
mkizka <g.mkizka@gmail.com>
2024-05-08 22:44:44 +0900

    husky

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -21,7 +21,7 @@ export default function handleRequest(
   remixContext: EntryContext,
   // This is ignored so we can keep it in the template for visibility.  Feel
   // free to delete this parameter in your app if you're not using it!
-   
+
   loadContext: AppLoadContext,
 ) {
   return isbot(request.headers.get("user-agent") || "")
