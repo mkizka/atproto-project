4bff9f4a1de7b283926ec2f0b4097385ce74bf22
mkizka <g.mkizka@gmail.com>
2024-05-11 17:48:37 +0900

    shadcn/ui

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,23 @@
 import { mkizka } from "@mkizka/eslint-config";
+import tailwindcss from "eslint-plugin-tailwindcss";
 
 export default [
   {
     ignores: ["build/", "atproto/", "app/generated/"],
+  },
+  {
+    plugins: { tailwindcss },
+    rules: {
+      ...tailwindcss.configs.recommended.rules,
+      "tailwindcss/classnames-order": "error",
+      "tailwindcss/no-custom-classname": [
+        "error",
+        {
+          callees: ["cn", "cva"],
+        },
+      ],
+      "tailwindcss/migration-from-tailwind-2": "error",
+    },
   },
   ...mkizka(),
 ];
