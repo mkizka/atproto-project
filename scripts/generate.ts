45a299cdc32d9c8adeee95674372a457bce70c97
mkizka <g.mkizka@gmail.com>
2024-05-18 15:32:10 +0900

    fix generate

--- scripts/generate.ts
+++ scripts/generate.ts
@@ -1,12 +1,17 @@
 import { execSync } from "child_process";
+import fs from "fs";
 import { globSync } from "glob";
+
+const $ = (command: string) => execSync(command, { stdio: "inherit" });
+
+if (!fs.existsSync("./atproto/lexicons")) {
+  $(`git submodule update --init`);
+}
 
 const lexicons = [
   ...globSync("./lexicons/**/*.json"),
   ...globSync("./atproto/lexicons/com/atproto/repo/*.json"),
 ].join(" ");
-
-const $ = (command: string) => execSync(command, { stdio: "inherit" });
 
 $(`yes | pnpm lex gen-api ./app/generated/api ${lexicons}`);
 $(`yes | pnpm lex gen-server ./app/generated/server ${lexicons}`);
