f9ea6fc7f8a9a676abe21a038f7e33143663900d
mkizka <g.mkizka@gmail.com>
2024-05-18 15:56:36 +0900

    fix download atproto lexicons

--- scripts/generate.ts
+++ scripts/generate.ts
@@ -1,17 +1,18 @@
 import { execSync } from "child_process";
-import fs from "fs";
+import { downloadTemplate } from "giget";
 import { globSync } from "glob";
 
 const $ = (command: string) => execSync(command, { stdio: "inherit" });
 
-if (!fs.existsSync("./atproto/lexicons")) {
-  $(`git submodule update --init`);
-}
+await downloadTemplate(
+  "github:bluesky-social/atproto/lexicons/com/atproto/repo",
+  {
+    dir: "./lexicons/com/atproto/repo",
+    forceClean: true,
+  },
+);
 
-const lexicons = [
-  ...globSync("./lexicons/**/*.json"),
-  ...globSync("./atproto/lexicons/com/atproto/repo/*.json"),
-].join(" ");
+const lexicons = globSync("./lexicons/**/*.json").join(" ");
 
 $(`yes | pnpm lex gen-api ./app/generated/api ${lexicons}`);
 $(`yes | pnpm lex gen-server ./app/generated/server ${lexicons}`);
