dde988455d80fef33cf5268fdf74da3297020998
mkizka <g.mkizka@gmail.com>
2024-05-26 02:34:18 +0900

    strict mode

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -5,15 +5,15 @@
  */
 
 import { RemixBrowser } from "@remix-run/react";
+import { StrictMode } from "react";
 import { startTransition } from "react";
 import { hydrateRoot } from "react-dom/client";
 
 startTransition(() => {
   hydrateRoot(
     document,
-    // TODO: 元に戻す
-    // <StrictMode>
-    <RemixBrowser />,
-    // </StrictMode>,
+    <StrictMode>
+      <RemixBrowser />
+    </StrictMode>,
   );
 });
