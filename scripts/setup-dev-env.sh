9f9998f5d3ee73e9e9ad9d3ab5aa021fbf6c2832
mkizka <g.mkizka@gmail.com>
2024-06-16 19:49:50 +0900

    replace submodule to giget

--- scripts/setup-dev-env.sh
+++ scripts/setup-dev-env.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# git submoduleを使うとRailwayで動作しないため、gigetを使ってatprotoを取得する
+if [ ! -d atproto ]; then
+  pnpm giget gh:bluesky-social/atproto atproto
+fi
 cd atproto
 make deps
 make build
