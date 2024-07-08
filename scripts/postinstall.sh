203f7b6e22841b62aca634dbebe4cbd262ec9157
mkizka <g.mkizka@gmail.com>
2024-07-09 02:33:40 +0900

    fix

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -11,3 +11,5 @@ cp -r ./atproto/lexicons/com/atproto/repo ./lexicons/com/atproto/repo
 LEXICONS=$(find ./lexicons -name '*.json' -type f)
 echo y | pnpm lex gen-api ./app/generated/api $LEXICONS
 echo y | pnpm lex gen-server ./app/generated/server $LEXICONS
+
+pnpm prisma generate
